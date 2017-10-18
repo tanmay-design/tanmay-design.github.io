934fa677cc735acfc83b375c33e14384d2f34bf1
tanmay-design <tanmayjoshi2205@gmail.com>
2017-10-18 16:26:59 +0530

    fourth-final

--- scripts.js
+++ scripts.js
@@ -25,4 +25,11 @@ $(document).ready(function(){
       $(".circlee2").animate({"animation-duration": "1.5s"},1);
       $(".circlee3").animate({"animation-duration": "1.5s"},1);
     });
+
+    if (document.documentElement.clientWidth < 600) {
+  	   document.getElementByClassName("circlee2").setAttribute("r", 60 );
+       document.getElementByClassName("circlee1").setAttribute("r", 80 );
+       document.getElementByClassName("circlee3").setAttribute("r", 100 );
+     }
+
 });
